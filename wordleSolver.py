f653735eeefd6a65769be4e58380b1f7cd470b34
jonkatzur <jkatzur@gmail.com>
2022-04-25 11:52:30 -0400

    Added many simulations. Better documentation.

--- wordleSolver.py
+++ wordleSolver.py
@@ -4,7 +4,14 @@ from wordfreq import get_frequency_dict
 import pandas
 
 # Implements wordle solver for wordle puzzle of n_letters
-# This first solver just ranks words by their wordfreq
+# This initial solver works on 'hard mode' only (only suggests words that are still possible)
+# And ranks guesses by the word frequency and likelihood of the letters + position of letters
+# In the remaining possible words
+# The composite rank scores the various features based on simulations I ran - check out  wordleSimulator.py
+
+# This can be improved by (1) ranking guesses by entropy (value of guess in reducing universe of potential words)
+# and (2) incorporating prior guesses/results to dynamically improve model weights
+# and (3) using the actual wordle corpus of words. I found it more fun to make it more general
 class wordleSolver:
     def __init__(self, n_letters: int):
         self.n_letters = n_letters
@@ -21,6 +28,7 @@ class wordleSolver:
 
         self.update_state()        
         
+    # Load potential words for this length. If I've already downloaded the corpus re-use it. 
     def load_start_words(self):
         try:
             return pandas.read_pickle(f"./start_words/start_words_{self.n_letters}_letters.pkl")
@@ -29,7 +37,7 @@ class wordleSolver:
             pos_words.to_pickle(f"./start_words/start_words_{self.n_letters}_letters.pkl")
             return pos_words
 
-    # Return list of all possible words of n_letter length as a dict with wordfreq
+    # Return list of all possible words of n_letter length as a dict including wordfreq
     def possible_words(self) -> List[int]:
         freq_dict = get_frequency_dict('en', wordlist='best')
         n_letter_words = []
@@ -38,9 +46,10 @@ class wordleSolver:
                 n_letter_words.append(w)
         df = pandas.DataFrame(n_letter_words)
         df.columns = ['word', 'freq']
-        return df
+        # arbitrary to take top 20000 words. I chose not to use the wordle corpus so this limits to words people know
+        return df.head(20000)
     
-    # Given a guess and a response this updates state including updating possible_words
+    # Given a guess and a response update state including updating possible_words and the various scores
     def process_guess(self, guess: str, response: List[int]) -> dict:
         self.guesses.append({'guess': guess, 'response': response})
         for i, r in enumerate(response):
@@ -54,7 +63,10 @@ class wordleSolver:
                 self.pos_yes[i].add(guess[i])
 
         self.possible_words = self.possible_words[self.possible_words.iloc[:,0:].apply(self.word_in, axis=1)]
-        self.update_state()
+        # Address case when only 1 possible word... in that case just tell them that 1 word! 
+        if len(self.possible_words) != 1:
+            self.update_state()
+
 
     # Package the non-turn based state updates for use during init and as part of turn processing
     def update_state(self):
@@ -74,7 +86,8 @@ class wordleSolver:
         self.possible_words['distinct_letters'].clip(0, 1)
 
 
-    # This is the method that is overridden in more advanced solvers
+    # This uses the remaining words and features to propose a single guess
+    # Arguments are weights to various features. Should be overridden as we learn moer
     def next_guess(self, sort_on:str = 'model_rank', model_params:dict = \
                     {'freq': 1, 'letter_score_by_word': 1, 'letter_score_by_freq': 1,
                     'letter_score_pos_perc': 1, 'letter_score_pos_freq': 1, 'distinct_letters': 1}) -> str:
@@ -82,7 +95,8 @@ class wordleSolver:
         self.possible_words['model_rank'] = sum(model_params[weight] * self.possible_words[weight] for weight in model_params.keys())
         return self.possible_words.sort_values(sort_on, ascending=False).iloc[0]['word']
 
-    # Return top n rows by some method/params. Use same params as guess to evaluate consistently!
+    # Return top n rows ranked by the feature weights
+    # Useful to expose to people who want to pick their guess word
     def top_n_by(self, n:int = 20, sort_on:str = 'model_rank', model_params:dict = \
                     {'freq': 1, 'letter_score_by_word': 1, 'letter_score_by_freq': 1,
                     'letter_score_pos_perc': 1, 'letter_score_pos_freq': 1, 'distinct_letters': 1}):
@@ -93,6 +107,7 @@ class wordleSolver:
 
     
     # Calculates the per-letter likelihood of each letter in the remaining possible words
+    # This can be thought of as what percent of remaining words contain each letter
     def update_letter_scores(self) -> float:
         words_by_letter = {}
         words_by_letter_weighted = {}
@@ -114,7 +129,8 @@ class wordleSolver:
                     words_by_letter[l] = 1
                     words_by_letter_weighted[l] = row[1]
 
-            # Specific position
+            # Specific position. This asks the question what percent of remaining possible words
+            # have this letter in a specific position (e.g 'a' in the second position)
             for l in range(self.n_letters):
                 if row[0][l] in words_by_letter_pos_count[l]:
                     words_by_letter_pos_count[l][row[0][l]] += 1
@@ -181,7 +197,7 @@ class wordleSolver:
 
 
 
-# For when you are running the solver via command line
+# This main runs the solver via command line
 if __name__ == '__main__':
     # Pick a length of game we're trying to solve
     while True:
@@ -206,11 +222,14 @@ if __name__ == '__main__':
     print(f"\n\n---- Starting wordle solver for {n_letters} letter word ----")
 
     turn = 1
+    # Note, I picked specific weights for each feature here, which change over time depending on which turn
+    # These can be improved by more careful analysis and making a real learning system
+    # Also should package the structure for .next_guess and .top_n_by so they use the same weights without copy pasta
     while game_on:
         print(f"Currently potential matched words is: {len(wordle_solver.possible_words)}")
-        print(f"Top guess based on our model is: {wordle_solver.next_guess()}")
+        print(f"Top guess based on our model is: {wordle_solver.next_guess(sort_on='model_rank', model_params={'freq':0.2 * turn, 'letter_score_by_word': 0.3/turn, 'letter_score_by_freq': .3/turn, 'letter_score_pos_perc': 1.5/turn, 'letter_score_pos_freq': 1.5/turn, 'distinct_letters': 1.2})}")
 
-        # Would you like to see more
+        # Would you like to see more possible words?
         while True:
             see_top = input(f"Would you like to see the top 20 possible words? Y(es) or N(o)? {'' : >3}").upper()
             if see_top == 'Y' or see_top == 'N':
@@ -220,7 +239,7 @@ if __name__ == '__main__':
 
         if see_top == 'Y':
             print(f"\nTop 20 suggested guesses are:")
-            print(wordle_solver.top_n_by(n=20, sort_on='model_rank', model_params={'freq':0.2 * turn, 'letter_score_by_word': 0.4/turn, 'letter_score_by_freq': .5/turn, 'letter_score_pos_perc': .7/turn, 'letter_score_pos_freq': 0.7/turn, 'distinct_letters': 0.2/turn}))
+            print(wordle_solver.top_n_by(n=20, sort_on='model_rank', model_params={'freq':0.2 * turn, 'letter_score_by_word': 0.3/turn, 'letter_score_by_freq': 0.3/turn, 'letter_score_pos_perc': 1.5/turn, 'letter_score_pos_freq': 1.5/turn, 'distinct_letters': 1.2}))
             print("\n")
         
         # Input guess
